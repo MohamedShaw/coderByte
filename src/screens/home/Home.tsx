4b7aab1ccb8cf950da32cd5caa69abf321c89227
Mohamed Shawk <m.shawky@crocoit.com>
2021-09-07 17:24:53 +0200

    first  commit

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
-import {RecordVideo} from '../media';
 import {styles} from './styles';
 
 interface Props {}
 export function Home({}: Props) {
   return (
     <View style={styles.container}>
-      <RecordVideo />
+      <View />
     </View>
   );
 }
